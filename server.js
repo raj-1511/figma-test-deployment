083e4b788c059cb241739363362f1288d1561b40
raj-1511 <rajp.spaceo@gmail.com>
2025-08-28 16:57:46 +0530

    Add server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// Default route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`View your site at http://localhost:${PORT}`);
+});
